f05da79f3af03130a3c111d84563445a49e18caa
Steve Larsen <slarsen-3@att.net>
2022-08-14 15:21:31 -0500

    Header and Footer Links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,47 @@
-import { Navbar, NavbarBrand } from 'reactstrap';
+import {
+  Collapse,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavItem,
+  NavbarToggler,
+} from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
 import NucampLogo from '../app/assets/img/logo.png';
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <Navbar dark color='primary' sticky='top' expand='md'>
       <NavbarBrand href='/'>
         <img src={NucampLogo} alt='NuCamp Logo' />
       </NavbarBrand>
+      <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
+      <Collapse isOpen={menuOpen} navbar>
+        <Nav className='ms-auto' navbar>
+          <NavItem>
+            <NavLink className='nav-link' to='/'>
+              <i className='fa fa-home fa-lg' /> Home
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className='nav-link' to='/directory'>
+              <i className='fa fa-list fa-lg' /> Directory
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className='nav-link' to='/about'>
+              <i className='fa fa-info fa-lg' /> About
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className='nav-link' to='/contact'>
+              <i className='fa fa-address-card fa-lg' /> Contact
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </Collapse>
     </Navbar>
   );
 };

aee9cc719e4d0d754e011bb4db3349fd075b9eaf
Steve Larsen <slarsen-3@att.net>
2022-08-19 11:27:41 -0500

    Week 2 workshop

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import './App.css';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import AboutPage from './pages/AboutPage';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path='contact' element={<ContactPage />} />
         <Route path='directory' element={<CampsitesDirectoryPage />} />
         <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} />
+        <Route path='about' element={<AboutPage />} />
       </Routes>
       <Footer />
     </div>

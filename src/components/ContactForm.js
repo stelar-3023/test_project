bc8178f2a2a57714780970a8cbda2139ff510ffa
Steve Larsen <slarsen-3@att.net>
2022-08-27 15:45:44 -0500

    validateContactForm

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,5 +1,6 @@
 import { Button, Label, Col, FormGroup } from 'reactstrap';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { validateContactForm } from '../utils/validateContactForm';
 
 const ContactForm = () => {
   const handleSubmit = (values, { resetForm }) => {
@@ -19,6 +20,7 @@ const ContactForm = () => {
         feedback: '',
       }}
       onSubmit={handleSubmit}
+      validate={validateContactForm}
     >
       <Form>
         <FormGroup row>
@@ -31,6 +33,9 @@ const ContactForm = () => {
               name='firstName'
               placeholder='First Name'
             />
+            <ErrorMessage name='firstName'>
+              {(msg) => <p className='text-danger'>{msg}</p>}
+            </ErrorMessage>
           </Col>
         </FormGroup>
         <FormGroup row>
@@ -43,6 +48,9 @@ const ContactForm = () => {
               name='lastName'
               placeholder='Last Name'
             />
+            <ErrorMessage name='lastName'>
+              {(msg) => <p className='text-danger'>{msg}</p>}
+            </ErrorMessage>
           </Col>
         </FormGroup>
         <FormGroup row>
@@ -55,6 +63,9 @@ const ContactForm = () => {
               name='phoneNum'
               placeholder='Phone Number'
             />
+            <ErrorMessage name='phoneNum'>
+              {(msg) => <p className='text-danger'>{msg}</p>}
+            </ErrorMessage>
           </Col>
         </FormGroup>
         <FormGroup row>
@@ -63,6 +74,9 @@ const ContactForm = () => {
           </Label>
           <Col md='10'>
             <Field className='form-control' name='email' placeholder='Email' />
+            <ErrorMessage name='email'>
+              {(msg) => <p className='text-danger'>{msg}</p>}
+            </ErrorMessage>
           </Col>
         </FormGroup>
         <FormGroup row>

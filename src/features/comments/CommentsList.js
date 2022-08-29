98d380960b90f7f8075e254a54bec90ae3f9439c
Steve Larsen <slarsen-3@att.net>
2022-08-29 14:31:20 -0500

    Week3 Workshop

--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -1,6 +1,8 @@
 import { Col } from 'reactstrap';
 import Comment from './Comment';
+import CommentForm from './CommentForm';
 import { selectCommentsByCampsiteId } from './commentsSlice';
+
 
 const CommentsList = ({ campsiteId }) => {
   const comments = selectCommentsByCampsiteId(campsiteId);
@@ -11,6 +13,7 @@ const CommentsList = ({ campsiteId }) => {
         {comments.map((comment) => {
           return <Comment key={comment.id} comment={comment} />;
         })}
+        <CommentForm campsiteId={campsiteId} />
       </Col>
     );
   }

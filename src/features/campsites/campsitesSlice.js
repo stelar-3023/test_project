414371d4a7cfb725e1b31885a43fce301af6f1d5
Steve Larsen <slarsen-3@att.net>
2022-08-31 18:15:15 -0500

    Redux campsitesSlice

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,8 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit';
 import { CAMPSITES } from '../../app/shared/CAMPSITES.js';
+
+const initialState = {
+  campsitesArray: CAMPSITES,
+}
+
+const campsitesSlice = createSlice({
+  name: 'campsites',
+  initialState,
+});
 
 export const selectAllCampsites = () => {
   return CAMPSITES;
 };
+
+export const campsitesReducer = campsitesSlice.reducer;
 
 // export const selectRandomCampsite = () => {
 //   return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
